734a181b9a8dcbfdb22ee11f9ff901c3cdd169d6
Sergey Mysov <s.mysov@list.ru>
2020-10-09 23:24:55 +0400

    feat: finished task dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  if (!Array.isArray(arr)) return false;
+  return arr
+    .filter((string) => typeof string === "string")
+    .map((string) => string.trim().slice(0, 1).toUpperCase())
+    .sort()
+    .join('');
 };
